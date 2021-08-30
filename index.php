5747e1dbd776bb6c2fec63488487543f3950a1e0
Rouji <a@r0.at>
2021-08-30 12:21:56 +0200

    unfuck code golf

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ class CONFIG
 
     public static function SITE_URL() : string
     {
-        $proto = $_SERVER['HTTPS'] ?? true ? 'http' : 'https';
+        $proto = ($_SERVER['HTTPS'] ?? 'off') == 'on' ? 'https' : 'http';
         return "$proto://{$_SERVER['HTTP_HOST']}/";
     }
 };
